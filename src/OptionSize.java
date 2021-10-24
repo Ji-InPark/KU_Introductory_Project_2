dac3a19d9e4140c3617732267dcb61a8c84b4d3b
JinPark <jin225675@naver.com>
2021-10-24 22:32:37 +0900

    implement FileList

--- src/OptionSize.java
+++ src/OptionSize.java
@@ -23,46 +23,46 @@ public class OptionSize {
     public void Analyze() throws IOException {
         if(NP == -1)        // N보다 작은 것 탐색
         {
-            for(int i = 0; i < fileList.size(); i++)        // 여기서 fileList는 순회가 끝난 파일들의 어래이리스트
+            for(int i = 0; i < fileList.getSize(); i++)        // 여기서 fileList는 순회가 끝난 파일들의 어래이리스트
             {
                 File file = fileList.getFile(i);
                 if(Files.size(file.toPath()) < NP * Math.pow(1024, kMG))
                 {
-                    fileList.setFind(i, true);              // 여기서 setFind(index, boolean)는 해당 파일이 조건에 충족하는지(true, false)를 알려줌 (fileList 클래스에 boolean ArrayList 혹은 Array 가 있음)
+                    fileList.setResult(i, true);              // 여기서 setFind(index, boolean)는 해당 파일이 조건에 충족하는지(true, false)를 알려줌 (fileList 클래스에 boolean ArrayList 혹은 Array 가 있음)
                 }
                 else
                 {
-                    fileList.setFind(i, false);
+                    fileList.setResult(i, false);
                 }
             }
         }
         else if(NP == 0)    // N과 정확히 같은 것 탐색
         {
-            for(int i = 0; i < fileList.size(); i++)        // 여기서 fileList는 순회가 끝난 파일들의 어래이리스트
+            for(int i = 0; i < fileList.getSize(); i++)        // 여기서 fileList는 순회가 끝난 파일들의 어래이리스트
             {
                 File file = fileList.getFile(i);
                 if(Files.size(file.toPath()) == NP * Math.pow(1024, kMG))
                 {
-                    fileList.setFind(i, true);              // 여기서 setFind(index, boolean)는 해당 파일이 조건에 충족하는지(true, false)를 알려줌 (fileList 클래스에 boolean ArrayList 혹은 Array 가 있음)
+                    fileList.setResult(i, true);              // 여기서 setFind(index, boolean)는 해당 파일이 조건에 충족하는지(true, false)를 알려줌 (fileList 클래스에 boolean ArrayList 혹은 Array 가 있음)
                 }
                 else
                 {
-                    fileList.setFind(i, false);
+                    fileList.setResult(i, false);
                 }
             }
         }
         else                // N보다 큰 것 탐색
         {
-            for(int i = 0; i < fileList.size(); i++)        // 여기서 fileList는 순회가 끝난 파일들의 어래이리스트
+            for(int i = 0; i < fileList.getSize(); i++)        // 여기서 fileList는 순회가 끝난 파일들의 어래이리스트
             {
                 File file = fileList.getFile(i);
                 if(Files.size(file.toPath()) > NP * Math.pow(1024, kMG))
                 {
-                    fileList.setFind(i, true);              // 여기서 setFind(index, boolean)는 해당 파일이 조건에 충족하는지(true, false)를 알려줌 (fileList 클래스에 boolean ArrayList 혹은 Array 가 있음)
+                    fileList.setResult(i, true);              // 여기서 setFind(index, boolean)는 해당 파일이 조건에 충족하는지(true, false)를 알려줌 (fileList 클래스에 boolean ArrayList 혹은 Array 가 있음)
                 }
                 else
                 {
-                    fileList.setFind(i, false);
+                    fileList.setResult(i, false);
                 }
             }
         }
