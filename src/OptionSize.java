c674cc68556030b8a7602ad4244a8d1ddfd60756
JinPark <jin225675@naver.com>
2021-10-24 16:54:49 +0900

    sketch size option

---
+++ src/OptionSize.java
@@ -0,0 +1,47 @@
+public class OptionSize {
+
+    FileList fileList;      // 순회된 file들을 저장하는 클래스
+    int NP, N, kMG;         // (NP: -1은 -, 0은 생략, 1은 +), N: 입력 숫자, (kMG: 1은 k, 2는 M, 3은 G)
+
+
+    public OptionSize(FileList fileList)
+    {
+        this.fileList = fileList;
+    }
+
+    public void SetOption(int NP, int N, int kMG)
+    {
+        this.NP = NP;
+        this.N = N;
+        this.kMG = kMG;
+    }
+
+    public void Analyze()
+    {
+        if(NP == -1)        // N보다 작은 것 탐색
+        {
+            /*
+                for(int i = 0; i < fileList.size(); i++)        // 여기서 fileList는 순회가 끝난 파일들의 어래이리스트
+                {
+                    File temp_file = fileList.getFile(i);
+                    if(temp_file.getSize() < NP * Math.pow(1024, kMG))
+                    {
+                        fileList.setFind(i, true);              // 여기서 setFind는 해당 파일이 조건에 충족함을 알려줌 (fileList 클래스에 boolean ArrayList 혹은 Array 가 있음)
+                    }
+                    else
+                    {
+                        fileList.setFind(i, false);
+                    }
+                }
+            */
+        }
+        else if(NP == 0)    // N과 정확히 같은 것 탐색
+        {
+
+        }
+        else                // N보다 큰 것 탐색
+        {
+
+        }
+    }
+}
