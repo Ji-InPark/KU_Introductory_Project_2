8020339faa7916fd3181426af7dce531997ece49
GeonNamGung <rndrjs123@naver.com>
2021-10-26 03:29:10 +0900

    refactor OptionMaxdepth

--- src/src/main/java/src/OptionMaxdepth.java
+++ src/src/main/java/src/OptionMaxdepth.java
@@ -1,33 +1,36 @@
 package src;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.io.File;
 
 public class OptionMaxdepth implements Option {
-    private ArrayList<File> fileList;
     private File root;
-    int depth;
+    private int depth;
 
-    public OptionMaxdepth(ArrayList<File> fileList, int depth){
-        this.fileList = fileList;
+    public OptionMaxdepth(File root, int depth){
         this.depth = depth;
-        this.root = fileList.get(0);
+        this.root= root;
+
         checkArg();
     }
 
     @Override
     public List<File> analyze(){
-        ArrayList<File> result = maxdepth(this.root, this.depth);
+        ArrayList<File> result = maxdepth(this.root, this.depth+1);
         return result;
     }
 
     @Override
     public void checkArg() throws IllegalArgumentException {
         if(this.depth < 0){
-            throw new IllegalArgumentException("depth must be zero or more");
+            throw new IllegalArgumentException("maxdepth값은 0 이상의 정수로 주어져야 합니다");
+        }
+        if(!root.exists()){
+            throw new IllegalArgumentException("유효하지 않은 파일 경로입니다");
+        }
+        if(!root.isDirectory()){
+            throw new IllegalArgumentException("해당 경로는 디렉터리가 아닙니다");
         }
     }
 
@@ -38,7 +41,7 @@ public class OptionMaxdepth implements Option {
 
 
     public ArrayList<File> maxdepth(File file, int tmp_depth){
-        ArrayList<File> tmp = new ArrayList<File>();
+        ArrayList<File> tmp = new ArrayList<>();
 
         if(file != null) {
             tmp.add(file);
--- src/src/main/java/src/demo/OptionMaxdepthTypeDemo.java
+++ src/src/main/java/src/demo/OptionMaxdepthTypeDemo.java
@@ -1,81 +1,21 @@
 package src.demo;
 
-import src.FileList;
-import src.HelpMsg;
 import src.OptionMaxdepth;
-import src.OptionType;
+import src.Utils;
 
-import java.util.ArrayList;
 import java.io.File;
 
 public class OptionMaxdepthTypeDemo {
-    private FileList fileList;
-    private ArrayList<File> result;
-    public void filter(String path){
-        // this method find a certain File class through the input
-        // input can be divided into one or more parts
-        // [path] [expressions1] [expression2....]
-        // we can get path and File Node from this
-
-    }
-    public static void printArrayList(ArrayList<String> list){ // 임시로 arraylist 출력하는 함수
-        for(int i = 0; i < list.size(); i++)
-            System.out.println(list.get(i));
-    }
 
     public static void main(String[] args){
-        HelpMsg helpMsg = new HelpMsg();
-        FileList fileList = new FileList(".");
-        // it will be the args[0]
+        File rootFile= new File(".");
 
-        OptionMaxdepth options1 = new OptionMaxdepth(fileList);
-        OptionMaxdepth options2 = new OptionMaxdepth(fileList);
-        OptionType options3 = new OptionType(fileList);
-        OptionType options4 = new OptionType(fileList);
-        System.out.println("test1");
-        printArrayList(options1.maxdepth(2));
-        System.out.println("test2");
-        printArrayList(options2.maxdepth(3));
+        OptionMaxdepth optionMaxdepth= new OptionMaxdepth(rootFile, 0);
+        System.out.println("depth 0===========");
+        Utils.printResult(optionMaxdepth.analyze());
 
-        /*
-        Result is as follows:
-            test1
-            /
-            /1_0
-            /1_0/2_0
-            /1_0/2_1
-            /1_0/2_2
-            /1_1
-            /1_1/2_3
-            /1_1/2_4
-            /1_1/2_5
-            test2
-            ./
-            ./2_3
-            ./2_4
-            ./2_5
-            ./2_5/3_1
-        */
-        System.out.println("\n\n");
-        System.out.println("test3");
-        printArrayList(options3.type("d"));
-        // -> $ ./cfind / -type d (starts from root)
-        System.out.println("test4");
-        printArrayList(options4.type("f"));
-        // -> $ ./cfind . -type f (starts from /1_1)
-
-        /*
-        Result is as follows:
-            test3
-            /
-            /1_0
-            /1_1
-            /1_1/2_5
-            /1_1/2_5/3_1
-            test4
-            ./2_3
-            ./2_4
-            ./2_5/3_2
-        */
+        optionMaxdepth= new OptionMaxdepth(rootFile, 1);
+        System.out.println("depth 1===========");
+        Utils.printResult(optionMaxdepth.analyze());
     }
 }
